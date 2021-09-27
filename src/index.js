5c1e860694280679915163fad42e80ced6a4a8ff
ivanbilotserkivskiy <ivanbilotserkivskiy@gmail.com>
2021-09-27 20:46:30 +0300

    as

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './sass/main.scss';
+import pokemonTpl from './templates/handle.hbs'
+const containerRef = document.querySelector('.container')
+const r = fetch('https://pokeapi.co/api/v2/pokemon/3/').then(response => {
+    console.log(response.json)
+    return response.json()
+}).then(pokemon => {
+    console.log(pokemon)
+    const markup = pokemonTpl(pokemon)
+    console.log(markup)
+    containerRef.innerHTML = markup
+}).catch(error => {
+    console.log(error)
+})
