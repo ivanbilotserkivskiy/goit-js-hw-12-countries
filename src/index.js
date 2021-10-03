8b0a56caa875fee3a35eedea0ee08085d77f2636
ivanbilotserkivskiy <ivanbilotserkivskiy@gmail.com>
2021-10-03 12:44:55 +0300

    a

--- src/index.js
+++ src/index.js
@@ -21,21 +21,25 @@ function onSearch (e) {
 
   fetchCountries(searchQuery).then(data => {
        array = data.length
-       console.log(data)
-       console.log(data.length)
-       console.log(array)
        if(array > 10){
+        alert({
+          text: 'need to clarify request'
+        });
         fetchCountries(searchQuery).then(appendListMarkup)
+
       }
       else if (array >=2 && array < 10){
         fetchCountries(searchQuery).then(appendListMarkup)
+        
       }
       else if (array === 1){
+        alert({
+          text: 'your welcome dear!'
+        });
+      
         fetchCountries(searchQuery).then(appendCountyMarkup)
       }
-      return array}).then()
-      console.log(array)
-
+      return array})
       
     }
    
