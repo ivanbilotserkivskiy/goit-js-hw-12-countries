00cbafc3b174ec75839c62b44cc87c479ac2eeaa
ivanbilotserkivskiy <ivanbilotserkivskiy@gmail.com>
2021-10-03 11:29:13 +0300

    a

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,10 @@
 
     export default function fetchCountries(searchQuery) {
     
-    return fetch(`https://restcountries.com/v2/name/${this.searchQuery}`)
+    return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
     .then(response => response.json())
     .then(data =>{
-      console.log(data)
+      // console.log(data)
       return data
     }).catch(error => console.log(error))
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
   import fetchCountries from './fetchCountries'
   import countryiesTpl from './templates/handle.hbs'
+  import listTpl from './templates/list.hbs'
   import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
   import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
 
@@ -8,59 +9,37 @@
 const inputRef = document.querySelector('.search-input')
 const listRef = document.querySelector('.countries-list')
 
-
+let b = 0;
 inputRef.addEventListener('input', _.debounce(onSearch,500))
 
 function onSearch (e) {
     
     e.preventDefault();
-    // listRef.innerHTML ='';
-
-    let searchQuery = inputRef.value
-    console.log(countriesApiService.fetchCountries())
-    countriesApiService.fetchCountries(searchQuery).then(appendCountyMarkup)
-
-    // (array => {
-    //   if(array.length >= 2 && array.length <= 10) {
-    //   alert({
-    //     text: 'Please refine your search'
-    //   });
-    // }
-    // else if(array.length > 10) {
-    //   alert({
-    //     text: 'Need more information about country'
-    //   });
+    listRef.innerHTML ='';
     
-    // }
-    // else if (array.length === 1) {
-    //   alert({
-    //     text: 'Your welcome dear'
-    //   });
-    // }
-    //        let result = array.map(elem => {
-            
-    //     if(array.length === 1){
-    //       return `
-    //         <h1>${elem.name}</h1>
-    //         <img src='${elem.flags.png}'>
-    //         <p>Capital:${elem.capital}</p>
-    //         <p>Population:${elem.population}<//p>
-    //        `
-    //     }
-    //     else {
-          
-    //     return`
-    //     <li class="list-element">
-    //       <p>${elem.name}</p>
-    //     </li> `
-    //     }
-    //   }).join('')
-      
-      // listRef.insertAdjacentHTML('afterbegin', result)
+    let searchQuery = inputRef.value
+  //  fetchCountries(searchQuery).then(appendCountyMarkup)
+  fetchCountries(searchQuery).then(data => {
+       b = data.length
+       console.log(data)
+      return b})
+      if(b>10){
+        fetchCountries(searchQuery).then(appendListMarkup)
+      }
+      else if (b>=2 && b<10){
+        fetchCountries(searchQuery).then(appendListMarkup)
+      }
+      else if (b === 1){
+        fetchCountries(searchQuery).then(appendCountyMarkup)
+      }
     }
-
+   
     
     function appendCountyMarkup(hits) {
       listRef.insertAdjacentHTML('beforeend', countryiesTpl(hits))
 
   }
+  function appendListMarkup(hits) {
+    listRef.insertAdjacentHTML('beforeend', listTpl(hits))
+
+}
